72c8c05d7f1a39599c97f894bb729d304d53f2e3
Andrew Le <s3820098@rmit.edu.vn>
2022-08-23 17:12:12 +0700

    2. MenuScreen 1: gradient background

--- ShinkeiSuijaku/Views/MenuScreenView.swift
+++ ShinkeiSuijaku/Views/MenuScreenView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct MenuScreenView: View {
     var body: some View {
-        Text("Menu View")
+        ZStack {
+            // blue gradient background
+            LinearGradient(
+                colors: [Color("MenuBg1"), Color("MenuBg2")],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            // circles in background
+            GeometryReader { proxy in
+                let size = proxy.size
+                
+                // make the background slightly darker
+                Color.black
+                    .opacity(0.7)
+                    .blur(radius: 200)
+                    .ignoresSafeArea()
+                
+                // first blurred cicle in background
+                Circle()
+                    .fill(Color.pink)
+                    .padding(50)
+                    .blur(radius: 120)
+                    .offset(x: -size.width / 1.8, y: -size.height / 5)
+                
+                // second blurred cicle in background
+                Circle()
+                    .fill(Color.blue)
+                    .padding(50)
+                    .blur(radius: 120)
+                    .offset(x: size.width / 1.8, y: -size.height / 2)
+                
+                // third blurred cicle in background
+                Circle()
+                    .fill(Color.green)
+                    .padding(50)
+                    .blur(radius: 120)
+                    .offset(x: size.width / 1.8, y: size.height / 2)
+                
+                // forth blurred cicle in background
+                Circle()
+                    .fill(Color.purple)
+                    .padding(60)
+                    .blur(radius: 120)
+                    .offset(x: -size.width / 1.8, y: size.height / 2)
+            }
+        }
     }
 }
 
