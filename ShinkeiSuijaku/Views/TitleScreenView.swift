e834f35ece99067962bc925ace728ee09eca5f2e
Andrew Le <s3820098@rmit.edu.vn>
2022-08-29 20:33:42 +0700

    18. add headers

--- ShinkeiSuijaku/Views/TitleScreenView.swift
+++ ShinkeiSuijaku/Views/TitleScreenView.swift
@@ -1,3 +1,23 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Le Nguyen Truong An
+  ID: s3820098
+  Last modified: 29/08/2022
+  Acknowledgement:
+    + https://stackoverflow.com/
+    + https://www.hackingwithswift.com/
+    + https://www.raywenderlich.com/
+    + https://github.com/
+    + https://www.swiftbysundell.com/
+    + https://www.chosic.com/
+    + https://mixkit.co/
+    + https://www.donnywals.com/
+    + https://thisdevbrain.com/
+*/
+
 //
 //  MenuScreenView.swift
 //  ShinkeiSuijaku
